72882d3fe166ee84138bb883723eaa9078738376
Alan <31026047+NIGHTMARE06@users.noreply.github.com>
2017-12-21 18:47:12 -0600

    Update SucesionFibonacci.java

--- SucesionFibonacci.java
+++ SucesionFibonacci.java
@@ -1,5 +1,3 @@
-package sucesion;
-
 import javax.swing.JOptionPane;
 
 /**
@@ -9,16 +7,16 @@ import javax.swing.JOptionPane;
 public class SucesionFibonacci {
     public static void main(String[] args) {
         int max = Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos numeros se mostraran?"));
-        int[] numbers = new int[max+2];
-        numbers[0] = 1;
-        System.out.println("1: " + numbers[0]);
+        long[] numbers = new long[max];
+        numbers[0] = 0;
         numbers[1] = 1;
-        System.out.println("2: " + numbers[1]);
         
-        for(int i = 0; i < max-2; i++) {
-            numbers[i+2] = numbers[i] + numbers[i+1];
-            System.out.println((i+3) + ": " + (numbers[i+2]));
+        for(int i = 2; i < max; i++) {
+            numbers[i] = numbers[i-1] + numbers[i-2];
+        }
+
+        for(int i = 0; i < max; i++) {
+            System.out.println((i+1) + ": " + numbers[i]);
         }
     }
-    
 }
